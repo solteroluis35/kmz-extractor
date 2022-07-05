7293f62df36fcfaf38ba0c7cce601935425b548b
Luis Soltero <solteroluis35@gmail.com>
2022-07-05 11:15:01 -0500

    Formatting

--- kmz_extractor.py
+++ kmz_extractor.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-"""Script for KMZ file data extraction to Lat/Lon and dbZ values."""
+'''Script for KMZ file data extraction to Lat/Lon and dbZ values.'''
 
 import argparse
 import os
@@ -17,10 +17,10 @@ SCRIPT_DIR = os.getcwd()
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
-    "-f", "--fileName", help="The KMZ file you want to analyze")
-arg_parser.add_argument("-o", "--outputFileName",
-                        help="The output name for the CSV file")
-arg_parser.add_argument("-k", "--keepWorkDir", help="To keep the workdir",
+    '-f', '--fileName', help='The KMZ file you want to analyze')
+arg_parser.add_argument('-o', '--outputFileName',
+                        help='The output name for the CSV file')
+arg_parser.add_argument('-k', '--keepWorkDir', help='To keep the workdir',
                         action=argparse.BooleanOptionalAction)
 args = arg_parser.parse_args()
 
@@ -39,19 +39,19 @@ else:
 if not os.path.exists(WORK_DIR):
     os.makedirs(WORK_DIR)
 copy(kmzFile, WORK_DIR)
-os.chdir("./"+WORK_DIR)
+os.chdir('./'+WORK_DIR)
 
 # Uncompress KMZ
 name, ext = os.path.splitext(kmzFile)
-os.rename(kmzFile, name + ".zip")
-zip_file = name + ".zip"
+os.rename(kmzFile, name + '.zip')
+zip_file = name + '.zip'
 
-with ZipFile(file=zip_file, mode="r", allowZip64=True) as file:
-    print("Uncompressing")
+with ZipFile(file=zip_file, mode='r', allowZip64=True) as file:
+    print('Uncompressing')
     file.extractall(path=os.curdir)
 
     extracted_image = next(
-        filter(lambda file: file.endswith(".png"), os.listdir()))
+        filter(lambda file: file.endswith('.png'), os.listdir()))
     os.rename(extracted_image, RADAR_IMAGE_FILE)
 
 # Get array from image
@@ -61,7 +61,7 @@ image_array = np.array(Image.open(RADAR_IMAGE_FILE))
 
 # Get KML file reference coordinates
 print('Getting coordinates')
-with open(KML_INFO_FILE, 'r', encoding="utf8") as kml:
+with open(KML_INFO_FILE, 'r', encoding='utf8') as kml:
     root = parser.parse(kml).getroot()
 
 north = root.Folder.GroundOverlay.LatLonBox.north.text
@@ -96,14 +96,27 @@ for i in np.arange(0, np.size(image_array, 0)):
 filtered_image_array = np.delete(filtered_image_array, 0, 0)
 
 # Reference points
-reference_points = np.array([[255, 255, 255, 78], [255, 225, 255, 73], [255, 200, 255, 68],
-                            [255, 128, 255, 63], [
-                                255, 0, 255, 58], [255, 0, 100, 53],
-                            [255, 0, 0, 48], [255, 85, 0, 43], [255, 170, 0, 38],
-                            [255, 200, 0, 33], [
-                                255, 255, 0, 28], [0, 150, 50, 23],
-                            [0, 175, 0, 18], [0, 255, 0, 13], [0, 255, 128, 10],
-                            [0, 255, 255, -10], [0, 0, 255, -31.5]])
+reference_points = np.array(
+    [
+        [255, 255, 255, 78],
+        [255, 225, 255, 73],
+        [255, 200, 255, 68],
+        [255, 128, 255, 63],
+        [255, 0, 255, 58],
+        [255, 0, 100, 53],
+        [255, 0, 0, 48],
+        [255, 85, 0, 43],
+        [255, 170, 0, 38],
+        [255, 200, 0, 33],
+        [255, 255, 0, 28],
+        [0, 150, 50, 23],
+        [0, 175, 0, 18],
+        [0, 255, 0, 13],
+        [0, 255, 128, 10],
+        [0, 255, 255, -10],
+        [0, 0, 255, -31.5]
+    ]
+)
 
 # Distance between reference points
 reference_points_distance = np.zeros(16)
@@ -125,7 +138,7 @@ for i in np.arange(0, 16):
         reference_points_distance[i]
 
     def rgb_to_dbz(red, green, blue):
-        """RGB to dBZ function"""
+        '''RGB to dBZ function'''
         if red <= 30:
             array = np.zeros(6)
             for i in np.arange(11, 17):
@@ -139,28 +152,42 @@ for i in np.arange(0, 16):
             lb_index = np.where(array == lb_distance)[0][0]
 
             if la_index == 0:
-                assigned_reflectivity = reference_points[11,
-                                                         3] - (lineal_factors[11]) * la_distance
+                assigned_reflectivity = (
+                    reference_points[11, 3]
+                    - (lineal_factors[11]) * la_distance
+                )
             elif la_index == 5:
-                assigned_reflectivity = reference_points[16,
-                                                         3] + (lineal_factors[15]) * la_distance
+                assigned_reflectivity = (
+                    reference_points[16, 3]
+                    + (lineal_factors[15]) * la_distance
+                )
             else:
                 if lb_index == la_index:
-                    assigned_reflectivity_with_la_distance = reference_points[11+la_index, 3] - \
-                        (lineal_factors[11 + la_index]) * la_distance
-                    assigned_reflectivity_with_lb_distance = reference_points[12+la_index, 3] + \
-                        (lineal_factors[11 + la_index]) * lb_distance
+                    assigned_reflectivity_with_la_distance = (
+                        reference_points[11+la_index, 3]
+                        - (lineal_factors[11 + la_index]) * la_distance
+                    )
+                    assigned_reflectivity_with_lb_distance = (
+                        reference_points[12+la_index, 3]
+                        + (lineal_factors[11 + la_index]) * lb_distance
+                    )
                     assigned_reflectivity = (
-                        assigned_reflectivity_with_la_distance +
-                        assigned_reflectivity_with_lb_distance)/2
+                        assigned_reflectivity_with_la_distance
+                        + assigned_reflectivity_with_lb_distance
+                    ) / 2
                 else:
-                    assigned_reflectivity_with_la_distance = reference_points[11+la_index, 3] + \
-                        (lineal_factors[10 + la_index]) * la_distance
-                    assigned_reflectivity_with_lb_distance = reference_points[10+la_index, 3] - \
-                        (lineal_factors[10 + la_index]) * lb_distance
+                    assigned_reflectivity_with_la_distance = (
+                        reference_points[11+la_index, 3]
+                        + (lineal_factors[10 + la_index]) * la_distance
+                    )
+                    assigned_reflectivity_with_lb_distance = (
+                        reference_points[10+la_index, 3]
+                        - (lineal_factors[10 + la_index]) * lb_distance
+                    )
                     assigned_reflectivity = (
-                        assigned_reflectivity_with_la_distance +
-                        assigned_reflectivity_with_lb_distance)/2
+                        assigned_reflectivity_with_la_distance
+                        + assigned_reflectivity_with_lb_distance
+                    ) / 2
 
         if red >= 225:
             array = np.zeros(11)
@@ -175,34 +202,51 @@ for i in np.arange(0, 16):
             lb_index = np.where(array == lb_distance)[0][0]
 
             if la_index == 10:
-                assigned_reflectivity = reference_points[10,
-                                                         3] + (lineal_factors[9]) * la_distance
+                assigned_reflectivity = (
+                    reference_points[10, 3]
+                    + (lineal_factors[9]) * la_distance
+                )
             elif la_index == 0:
-                assigned_reflectivity = reference_points[0,
-                                                         3] - (lineal_factors[0]) * la_distance
+                assigned_reflectivity = (
+                    reference_points[0, 3]
+                    - (lineal_factors[0]) * la_distance
+                )
             else:
                 if lb_index == la_index:
-                    assigned_reflectivity_with_la_distance = reference_points[la_index, 3] - (
-                        lineal_factors[la_index]) * la_distance
-                    assigned_reflectivity_with_lb_distance = reference_points[la_index+1, 3] + (
-                        lineal_factors[la_index]) * lb_distance
+                    assigned_reflectivity_with_la_distance = (
+                        reference_points[la_index, 3]
+                        - (lineal_factors[la_index]) * la_distance
+                    )
+                    assigned_reflectivity_with_lb_distance = (
+                        reference_points[la_index+1, 3]
+                        + (lineal_factors[la_index]) * lb_distance
+                    )
                     assigned_reflectivity = (
-                        assigned_reflectivity_with_la_distance +
-                        assigned_reflectivity_with_lb_distance)/2
+                        assigned_reflectivity_with_la_distance
+                        + assigned_reflectivity_with_lb_distance
+                    ) / 2
                 else:
-                    assigned_reflectivity_with_la_distance = reference_points[la_index, 3] + (
-                        lineal_factors[la_index - 1]) * la_distance
-                    assigned_reflectivity_with_lb_distance = reference_points[la_index - 1, 3] - \
-                        (lineal_factors[la_index - 1]) * lb_distance
+                    assigned_reflectivity_with_la_distance = (
+                        reference_points[la_index, 3]
+                        + (lineal_factors[la_index - 1]) * la_distance
+                    )
+                    assigned_reflectivity_with_lb_distance = (
+                        reference_points[la_index - 1, 3]
+                        - (lineal_factors[la_index - 1]) * lb_distance
+                    )
                     assigned_reflectivity = (
                         assigned_reflectivity_with_la_distance +
-                        assigned_reflectivity_with_lb_distance)/2
+                        assigned_reflectivity_with_lb_distance
+                    ) / 2
         return assigned_reflectivity
 
     dbz_matrix = np.zeros((np.size(filtered_image_array, 0), 3))
     for i in np.arange(0, np.size(filtered_image_array, 0)):
         dbz_matrix[i, 0] = rgb_to_dbz(
-            filtered_image_array[i, 0], filtered_image_array[i, 1], filtered_image_array[i, 2])
+            filtered_image_array[i, 0],
+            filtered_image_array[i, 1],
+            filtered_image_array[i, 2]
+        )
         dbz_matrix[i, 1] = filtered_image_array[i, 3]
         dbz_matrix[i, 2] = filtered_image_array[i, 4]
 
@@ -229,6 +273,6 @@ data_frame.to_csv(OUTPUT_NAME+'.csv')
 
 if args.keepWorkDir is None:
     copy(OUTPUT_NAME+'.csv', SCRIPT_DIR)
-    rmtree(SCRIPT_DIR + "/" + WORK_DIR)
+    rmtree(SCRIPT_DIR + '/' + WORK_DIR)
 
 print('Done.')
